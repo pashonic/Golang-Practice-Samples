793aa830152f881d9720f45facb898c2a44aa07b
Aaron Greene <pashonic@gmail.com>
2022-06-20 00:31:16 -0700

    Check point

--- Cribbage-Stat/main.go
+++ Cribbage-Stat/main.go
@@ -12,19 +12,43 @@ type cardSet []card
 
 func createDeck() (deck cardSet) {
 
-	deck = append(deck, card{Type: "Eight", Suit: "Heart", Value: 8})
-	deck = append(deck, card{Type: "Seven", Suit: "Heart", Value: 7})
-	return
+	deck = append(deck, card{Type: "Eight", Suit: "Heart", Value: 5})
+	deck = append(deck, card{Type: "Seven", Suit: "Heart", Value: 5})
+	deck = append(deck, card{Type: "Eight", Suit: "Heart", Value: 5})
+	deck = append(deck, card{Type: "Seven", Suit: "Heart", Value: 5})
+	deck = append(deck, card{Type: "Seven", Suit: "Heart", Value: 10})
+	return deck
 }
 
-type scoreCombo interface {
-	Score(cards *cardSet)
+func get15Score(deck cardSet) int {
+	valueSet := []int{}
+	for _, v := range deck {
+		valueSet = append(valueSet, v.Value)
+	}
+	return subsetSum(valueSet, 15) * 2
 }
 
-func (self *cardSet) Get15Score() int {
+func subsetSum(array []int, target int) int {
 	total := 0
-	for _, card := range *self {
-		total += card.Value
+
+	for i := 0; i < len(array)-1; i++ {
+		total += subsetSumHelp(array, target, []int{array[i]}, i+1, array[i])
+	}
+	return total
+}
+
+func subsetSumHelp(array []int, target int, pass []int, start int, sum int) int {
+	if sum == target {
+		return 1
+	}
+
+	if sum > target {
+		return 0
+	}
+
+	total := 0
+	for i := start; i < len(array); i++ {
+		total += subsetSumHelp(array, target, append(pass, array[i]), i+1, sum+array[i])
 	}
 	return total
 }
@@ -32,6 +56,5 @@ func (self *cardSet) Get15Score() int {
 func main() {
 	deck := createDeck()
 
-	fmt.Println(deck.Get15Score())
-
+	fmt.Println(get15Score(deck))
 }
