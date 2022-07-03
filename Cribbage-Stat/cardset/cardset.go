bbf7f74cb4c5615b3fbc8d4910ccd86cf1dc7804
Aaron Greene <pashonic@gmail.com>
2022-07-02 21:43:27 -0700

    add module

---
+++ Cribbage-Stat/cardset/cardset.go
@@ -0,0 +1,65 @@
+
+type rank int
+
+const (
+	ACE rank = iota
+	TWO
+	THREE
+	FOUR
+	FIVE
+	SIX
+	SEVEN
+	EIGHT
+	NINE
+	TEN
+	JOKER
+	QUEEN
+	KING
+)
+
+type suit int
+
+const (
+	HEART suit = iota
+	SPADE
+	DIAMOND
+	CLUB
+)
+
+type card struct {
+	rank  rank
+	suit  suit
+	value int
+}
+type cardSet []card
+
+func createDeck() (deck cardSet) {
+
+	values := map[rank]int{
+		ACE:   1,
+		TWO:   2,
+		THREE: 3,
+		FOUR:  4,
+		FIVE:  5,
+		SIX:   6,
+		SEVEN: 7,
+		EIGHT: 8,
+		NINE:  9,
+		TEN:   10,
+		JOKER: 10,
+		QUEEN: 10,
+		KING:  10,
+	}
+
+	suits := []suit{
+		HEART, SPADE, DIAMOND, CLUB,
+	}
+
+	for rank, value := range values {
+		for _, suit := range suits {
+			deck = append(deck, card{rank: rank, suit: suit, value: value})
+		}
+	}
+
+	return deck
+}
