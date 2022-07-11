674edb4fbed1137ae7f52ddb7f94dd447c0847e2
Aaron Greene <pashonic@gmail.com>
2022-07-10 19:13:35 -0700

    check point

--- Cribbage-Stat/main.go
+++ Cribbage-Stat/main.go
@@ -2,30 +2,75 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/pashonic/Golang-Practice-Samples/Cribbage-Stat/cribbage"
 )
 
+func getPercentString(numerator uint64, denominator uint64) string {
+	percent := (float64(numerator) / float64(denominator)) * 100
+	return fmt.Sprintf("%.2f%%", percent)
+}
+
 func main() {
 
-	// Create a deck of cards
-	deck := cribbage.CreateDeck()
+	var total15Score uint64 = 0
+	var totalRunScore uint64 = 0
+	var totalPairScore uint64 = 0
+	var totalFlushScore uint64 = 0
+	var totalNobScore uint64 = 0
+	var sampleCount int = 1000000
 
-	// Deal 6 cards
-	dealtHand := deck.Deal(6)
+	for i := 0; i < sampleCount; i++ {
 
-	// Get the best 4 cards that result in highest score
-	selectedHand := dealtHand.GetBestCards(4)
+		// Create a deck of cards
+		deck := cribbage.CreateDeck()
 
-	// Deal cut card
-	var cutCard cribbage.Card = deck.Deal(1)[0]
+		// Deal 6 cards
+		dealtHand := deck.Deal(6)
 
-	// Add cut card to deck
-	dealtHand = append(selectedHand, cutCard)
+		// Get the best 4 cards that result in highest score
+		selectedHand := dealtHand.GetBestCards(4)
 
-	// Get Scores
-	fmt.Println(dealtHand)
-	fmt.Println(cutCard)
-	fmt.Println(dealtHand.GetTotalScoreWithNob(&cutCard))
+		// Deal cut card
+		var cutCard cribbage.Card = deck.Deal(1)[0]
 
+		// Add cut card to deck
+		dealtHand = append(selectedHand, cutCard)
+
+		// Add 2 cards from deck
+		finalHand := append(dealtHand, deck.Deal(2)...)
+
+		total15Score += uint64(finalHand.GetFifteenScore())
+		totalRunScore += uint64(finalHand.GetRunScore())
+		totalPairScore += uint64(finalHand.GetPairScore())
+		totalFlushScore += uint64(finalHand.GetFlushScore())
+		totalNobScore += uint64(finalHand.GetNobScore(&cutCard))
+	}
+
+	totalScore := total15Score + totalRunScore + totalPairScore + totalFlushScore + totalNobScore
+
+	// Print result.
+	type varItem struct {
+		name  string
+		value uint64
+	}
+	varMap := []varItem{
+		{"15 Combo", total15Score},
+		{"Run", totalRunScore},
+		{"Pairs", totalPairScore},
+		{"Flush", totalFlushScore},
+		{"Nob Score", totalNobScore},
+	}
+
+	fmt.Println("Sample Count: ", sampleCount)
+	spacer := fmt.Sprintf("+%s+\n", strings.Repeat("-", 68))
+	fmt.Printf(spacer)
+	fmt.Printf("| %-20s | %-20s | %-20s |\n", "Score Type", "Points", "Percentage")
+	fmt.Printf(spacer)
+	for _, varItem := range varMap {
+		u64String := fmt.Sprintf("%v", varItem.value)
+		fmt.Printf("| %-20s | %-20s | %-20s |\n", varItem.name, u64String, getPercentString(varItem.value, totalScore))
+		fmt.Printf(spacer)
+	}
 }
